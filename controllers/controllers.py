f237d1c843b526a7aa3183451a6875d9385a3c8f
xgarciaber <xgarciaber@educacion.navarra.es>
2026-01-19 21:00:34 +0100

    Hecho model del servicio de reservas , ademas de controller, para obtener las reservas desde otras fuentes

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,22 +1,10 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
 
 
-# class Kayak-ya(http.Controller):
-#     @http.route('/kayak-ya/kayak-ya', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+from odoo import http
+from odoo.http import request
 
-#     @http.route('/kayak-ya/kayak-ya/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('kayak-ya.listing', {
-#             'root': '/kayak-ya/kayak-ya',
-#             'objects': http.request.env['kayak-ya.kayak-ya'].search([]),
-#         })
-
-#     @http.route('/kayak-ya/kayak-ya/objects/<model("kayak-ya.kayak-ya"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('kayak-ya.object', {
-#             'object': obj
-#         })
-
+class KayakController(http.Controller):
+    @http.route('/api/kayak/bookings' , type='json' , auth='public')
+    def get_bookings(self):
+        bookings = request.env['kayak.booking'].sudo().search_read([] , ['name' , 'start_date' , 'state'])
+        return {'status': 'success', 'data': bookings}
