ac13b4e2efefe76344674ccd3d6ea6d80e30d212
xgarciaber <xgarciaber@educacion.navarra.es>
2026-01-22 17:21:37 +0100

    Errores de lógica en los models arreglados, vulnerabilidad del controller solucionada

--- models/res_partner.py
+++ models/res_partner.py
@@ -3,5 +3,8 @@ from odoo import models, fields
 class ResPartner(models.Model): 
     _inherit = "res.partner"
     
-    booking_ids = fields.One2many("kayak.booking" , "partner_id" , 
-                                  string = "Historial de reservas ")
+    booking_ids = fields.One2many(
+        
+        "kayak.booking" , 
+        "partner_id" , 
+        string = "Historial de reservas ")
