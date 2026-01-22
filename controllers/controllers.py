70fba0f70fc2072286c01e08a9a7bd3252bf5ee1
xgarciaber <xgarciaber@educacion.navarra.es>
2026-01-22 16:31:19 +0100

    Análisis vulnerabilidades

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,6 +5,8 @@ from odoo.http import request
 
 class KayakController(http.Controller):
     @http.route('/api/kayak/bookings' , type='json' , auth='public')
+
+    #Este método es inseguro , una persona sin autenticar puede descargarse la base de datos, tenemos que darle una vuelta equipo. 
     def get_bookings(self):
         bookings = request.env['kayak.booking'].sudo().search_read([] , ['name' , 'start_date' , 'state'])
         return {'status': 'success', 'data': bookings}
